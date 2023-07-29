9a6de438c7b8925aae54070b441958f70efb3c51
guilherme.hahn <guilherme.f.h@hotmail.com>
2023-07-28 21:32:14 -0300

    Mapeada a primeira Cidade

--- src/main/java/com/senac/gerenciamento/GerenciamentoApplication.java
+++ src/main/java/com/senac/gerenciamento/GerenciamentoApplication.java
@@ -1,5 +1,6 @@
 package com.senac.gerenciamento;
 
+import com.senac.gerenciamento.model.Cidade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class GerenciamentoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciamentoApplication.class, args);
+
+
 	}
 
 }
